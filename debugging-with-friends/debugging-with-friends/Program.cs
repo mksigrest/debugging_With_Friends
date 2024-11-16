ba40fd3bd63c4a34904a09e4d23a4528c2a2c9a7
mksigrest <mksigrest@gmail.com>
2024-11-16 18:08:26 -0500

    int push
    starter files pushed

---
+++ debugging-with-friends/debugging-with-friends/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace debugging_with_friends
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
